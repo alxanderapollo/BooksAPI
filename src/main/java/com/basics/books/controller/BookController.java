f785e9631ea06a4b3aab33ced7e07108798f2eed
Alex Salazar <asalazar66@gatech.edu>
2025-04-29 14:01:43 -0400

    added query and path params

--- src/main/java/com/basics/books/controller/BookController.java
+++ src/main/java/com/basics/books/controller/BookController.java
@@ -2,6 +2,8 @@ package com.basics.books.controller;
 
 import com.basics.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -28,9 +30,34 @@ public class BookController {
     }
 
     // get mapping without any given api linke goes to our root 8080 page
+//    @GetMapping("/api/books")
+//    public List<Book> getBooks() {
+//        return books;
+//    }
+
+//    path parameters - dynamic parts of the url
+//    path varaible lets spring know that were passing a varaible that will match at the end of the url
+    @GetMapping ("/api/books/{title}")
+    public Book getBookByTitle(@PathVariable String title){
+        for (Book book : books){
+//            if there exists a book with that title we want return that book
+            if(book.getTitle().equalsIgnoreCase(title)) return book;
+        }
+//        otherwise no book with that title exsits return nothing
+        return null;
+    }
+
+//    Query parameter
     @GetMapping("/api/books")
-    public List<Book> getBooks() {
-        return books;
+    public List<Book> getBooks(@RequestParam(required = false)  String category){
+//        if we do not pass anything just return all the books
+        if (category == null) return books;
+//        otherwise create new list that returns a filter of the category that is passed in
+        List<Book> filteredBooks = new ArrayList<>();
+        for(Book book : books){
+            if(book.getCategory().equalsIgnoreCase(category)) filteredBooks.add(book);
+        }
+        return filteredBooks ;
     }
 
 }
